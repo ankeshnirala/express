2cbde2265b865da08fe68bd9f9e0f456dc77c161
ankesh <ankeshnirala@gmail.com>
2024-03-18 10:32:22 +0530

    major changes done express setup realted to go version 1.22

--- express.go
+++ express.go
@@ -2,14 +2,12 @@ package express
 
 import "net/http"
 
-type Middlewares []func(http.Handler) http.Handler
-
 type Router interface {
 	HFunc(pattern string, handler http.HandlerFunc)
 	U(middlewares ...func(http.Handler) http.Handler)
 	M(middlewares ...func(http.Handler) http.Handler) Router
 
-	Group(fn func(r Router)) Router
+	Group(prefix string) *MuxRouter
 }
 
 func NewRouter() *MuxRouter {
--- mux.router.go
+++ mux.router.go
@@ -9,64 +9,65 @@ import (
 
 var _ Router = &MuxRouter{}
 
+// MuxRouter implements the Router interface
 type MuxRouter struct {
 	mux         *http.ServeMux
-	inline      bool
-	handler     http.Handler
 	middlewares []func(http.Handler) http.Handler
-
-	tree *RouterTree
+	prefix      string
 }
 
+// newMuxRouter creates a new instance of MyRouter
 func newMuxRouter() *MuxRouter {
-	mux := &MuxRouter{
-		mux:  http.NewServeMux(),
-		tree: NewRouterTree(),
+	return &MuxRouter{
+		mux:    http.NewServeMux(),
+		prefix: "",
 	}
-
-	return mux
 }
 
-func (mx *MuxRouter) HFunc(pattern string, handler http.HandlerFunc) {
-	parts := strings.SplitN(pattern, " ", 2)
-
-	if len(parts) != 2 || parts[1][0] != '/' {
-		panic(fmt.Sprintf("express: routing pattern must begin with method name and '/' in '%s', eg. 'GET /'", pattern))
-	}
-
-	if !mx.inline && mx.handler == nil {
-		mx.updateRouteHandler()
-	}
-
-	var h http.Handler
-	if mx.inline {
-		mx.handler = http.HandlerFunc(mx.routeHTTP)
-		h = Chain(mx.middlewares...).Handler(handler)
-	} else {
-		h = handler
-	}
-
-	mx.tree.Insert(parts[1], parts[0], h)
+// HFunc registers an HTTP handler for a specific method and path
+func (r *MuxRouter) HFunc(pattern string, handler http.HandlerFunc) {
+	npattern := strings.Split(pattern, " ")[0] + " " + r.prefix + strings.Split(pattern, " ")[1]
+	r.mux.HandleFunc(npattern, func(w http.ResponseWriter, req *http.Request) {
+		r.chain(handler).ServeHTTP(w, req)
+	})
 }
 
-func (mx *MuxRouter) M(middlewares ...func(http.Handler) http.Handler) Router {
-	if !mx.inline && mx.handler == nil {
-		mx.updateRouteHandler()
+// U adds middleware to the router
+func (r *MuxRouter) U(middlewares ...func(http.Handler) http.Handler) {
+	r.middlewares = append(r.middlewares, middlewares...)
+}
+
+// M creates a new router with middleware
+func (r *MuxRouter) M(middlewares ...func(http.Handler) http.Handler) Router {
+	return &MuxRouter{
+		mux:         r.mux,
+		middlewares: append(r.middlewares, middlewares...),
+		prefix:      r.prefix,
+	}
+}
+
+func (r *MuxRouter) chain(endpoint http.HandlerFunc) http.Handler {
+	// Return ahead of time if there aren't any middlewares for the chain
+	if len(r.middlewares) == 0 {
+		return endpoint
 	}
 
-	var mws Middlewares
-	if mx.inline {
-		mws = make(Middlewares, len(mx.middlewares))
-		copy(mws, mx.middlewares)
+	// Wrap the end handler with the middleware chain
+	h := r.middlewares[len(r.middlewares)-1](endpoint)
+	for i := len(r.middlewares) - 2; i >= 0; i-- {
+		h = r.middlewares[i](h)
 	}
 
-	mws = append(mws, middlewares...)
+	return h
+}
 
-	im := &MuxRouter{
-		mux: mx.mux, inline: true, middlewares: mws, handler: mx.handler, tree: mx.tree,
+// Group creates a new router with a prefix
+func (r *MuxRouter) Group(prefix string) *MuxRouter {
+	return &MuxRouter{
+		mux:         r.mux,
+		middlewares: r.middlewares,
+		prefix:      r.prefix + prefix,
 	}
-
-	return im
 }
 
 type statusCapturer struct {
@@ -78,40 +79,15 @@ func (s *statusCapturer) WriteHeader(statusCode int) {
 	s.status = statusCode
 	s.ResponseWriter.WriteHeader(statusCode)
 }
-
-func (mx *MuxRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (r *MuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	start := time.Now()
 	capturer := &statusCapturer{ResponseWriter: w}
-	// Create a handler that represents the final route handler
-	finalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Retrieve the route information from the router tree
-		routeInfo := mx.tree.Find(r.URL.Path, r.Method)
 
-		// Check if the route is found
-		if routeInfo.Path == "" {
-			http.NotFound(w, r)
-			return
-		}
-
-		// Check if the method is allowed for the route
-		if routeInfo.Method == "" {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// Serve the request using the route handler
-		routeInfo.Handler.ServeHTTP(w, r)
-	})
-
-	// Execute middleware chain
-	handler := chain(mx.middlewares, finalHandler)
-
-	// Process the request using the middleware chain
-	handler.ServeHTTP(capturer, r)
+	r.mux.ServeHTTP(capturer, req)
 
 	duration := time.Since(start)
 
-	msg := fmt.Sprintf("%v | %v | %s | %s | %s", capturer.status, formatResponseTime(duration), strings.SplitN(r.RemoteAddr, ":", 2)[0], r.Method, r.URL.Path)
+	msg := fmt.Sprintf("%v | %v | %s | %s | %s", capturer.status, formatResponseTime(duration), strings.SplitN(req.RemoteAddr, ":", 2)[0], req.Method, req.URL.Path)
 
 	NewLogger().Info(msg)
 }
@@ -121,48 +97,4 @@ func formatResponseTime(duration time.Duration) string {
 		return fmt.Sprintf("%.2fµs", float64(duration.Microseconds()))
 	}
 	return fmt.Sprintf("%.2fms", float64(duration.Milliseconds()))
-}
-
-func (mx *MuxRouter) U(middlewares ...func(http.Handler) http.Handler) {
-	mx.middlewares = append(mx.middlewares, middlewares...)
-}
-
-func (mx *MuxRouter) Group(fn func(r Router)) Router {
-	im := mx.M()
-	if fn != nil {
-		fn(im)
-	}
-	return im
-}
-
-func (mx *MuxRouter) updateRouteHandler() {
-	mx.handler = chain(mx.middlewares, http.HandlerFunc(mx.routeHTTP))
-}
-
-func (mx *MuxRouter) routeHTTP(w http.ResponseWriter, r *http.Request) {
-	routePath := ""
-	if routePath == "" {
-		if r.URL.RawPath != "" {
-			routePath = r.URL.RawPath
-		} else {
-			routePath = r.URL.Path
-		}
-		if routePath == "" {
-			routePath = "/"
-		}
-	}
-
-	routeInfo := mx.tree.Find(routePath, r.Method)
-
-	if routeInfo.Path == "" {
-		mx.ServeHTTP(w, r)
-		return
-	}
-
-	if routeInfo.Method == "" {
-		mx.ServeHTTP(w, r)
-		return
-	}
-
-	routeInfo.Handler.ServeHTTP(w, r)
 }
