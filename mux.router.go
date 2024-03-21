898b29366e013f760ffa70af450a8c5ff2ad809a
ankesh <ankeshnirala@gmail.com>
2024-03-21 17:58:44 +0530

    registering routes feature added

--- mux.router.go
+++ mux.router.go
@@ -46,21 +46,6 @@ func (r *MuxRouter) M(middlewares ...func(http.Handler) http.Handler) Router {
 	}
 }
 
-func (r *MuxRouter) chain(endpoint http.HandlerFunc) http.Handler {
-	// Return ahead of time if there aren't any middlewares for the chain
-	if len(r.middlewares) == 0 {
-		return endpoint
-	}
-
-	// Wrap the end handler with the middleware chain
-	h := r.middlewares[len(r.middlewares)-1](endpoint)
-	for i := len(r.middlewares) - 2; i >= 0; i-- {
-		h = r.middlewares[i](h)
-	}
-
-	return h
-}
-
 // Group creates a new router with a prefix
 func (r *MuxRouter) Group(prefix string) *MuxRouter {
 	return &MuxRouter{
@@ -70,15 +55,6 @@ func (r *MuxRouter) Group(prefix string) *MuxRouter {
 	}
 }
 
-type statusCapturer struct {
-	http.ResponseWriter
-	status int
-}
-
-func (s *statusCapturer) WriteHeader(statusCode int) {
-	s.status = statusCode
-	s.ResponseWriter.WriteHeader(statusCode)
-}
 func (r *MuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	start := time.Now()
 	capturer := &statusCapturer{ResponseWriter: w}
@@ -90,11 +66,4 @@ func (r *MuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	msg := fmt.Sprintf("%v | %v | %s | %s | %s", capturer.status, formatResponseTime(duration), strings.SplitN(req.RemoteAddr, ":", 2)[0], req.Method, req.URL.Path)
 
 	NewLogger().Info(msg)
-}
-
-func formatResponseTime(duration time.Duration) string {
-	if duration < time.Millisecond {
-		return fmt.Sprintf("%.2fµs", float64(duration.Microseconds()))
-	}
-	return fmt.Sprintf("%.2fms", float64(duration.Milliseconds()))
 }
